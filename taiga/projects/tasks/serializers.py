22179ebc30acf95cbba63e60fb111dbca18c843f
Michael Jurke <fairnmicha@gmx.de>
2016-11-06 14:16:20 +0100

    Add project extra info to issues, tasks and milestones list serializers

--- taiga/projects/tasks/serializers.py
+++ taiga/projects/tasks/serializers.py
@@ -23,15 +23,18 @@ from taiga.base.neighbors import NeighborsSerializerMixin
 from taiga.mdrender.service import render as mdrender
 from taiga.projects.attachments.serializers import BasicAttachmentsInfoSerializerMixin
 from taiga.projects.mixins.serializers import OwnerExtraInfoSerializerMixin
+from taiga.projects.mixins.serializers import ProjectExtraInfoSerializerMixin
 from taiga.projects.mixins.serializers import AssignedToExtraInfoSerializerMixin
 from taiga.projects.mixins.serializers import StatusExtraInfoSerializerMixin
 from taiga.projects.notifications.mixins import WatchedResourceSerializer
 from taiga.projects.tagging.serializers import TaggedInProjectResourceSerializer
 from taiga.projects.votes.mixins.serializers import VoteResourceSerializerMixin
 
+
 class TaskListSerializer(VoteResourceSerializerMixin, WatchedResourceSerializer,
                          OwnerExtraInfoSerializerMixin, AssignedToExtraInfoSerializerMixin,
-                         StatusExtraInfoSerializerMixin, BasicAttachmentsInfoSerializerMixin,
+                         StatusExtraInfoSerializerMixin, ProjectExtraInfoSerializerMixin,
+                         BasicAttachmentsInfoSerializerMixin,
                          TaggedInProjectResourceSerializer, serializers.LightSerializer):
 
     id = Field()
